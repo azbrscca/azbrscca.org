f243e46a5b4f0b5a3fb52e4c259596f605e1835c
Jeff Israel <jeff.israel@gmail.com>
2018-06-04 20:18:08 -0500

    Restore cleanArray and cleanString which are needed in the results download

--- common/Functions.php
+++ common/Functions.php
@@ -15,6 +15,27 @@
       return $files;
     }
 
+    public static function cleanArray( $dirtyArray ) {	+    private static function removePrefix( &$item, $index, $start) {
+      $cleanArray = array();	+      $item = substr( $item, $start );
+      foreach( array_keys( $dirtyArray ) as $key ) {
+        if ( is_array( $dirtyArray[ $key ] ) ) {
+          $cleanArray[ $key ] = self::cleanArray( $dirtyArray[ $key ] );
+        } else {	
+          $cleanArray[ $key ] = self::cleanString( $dirtyArray[ $key ] );
+        }
+      }
+      return $cleanArray;
+    }
+
+    public static function cleanString( $dirty ) {
+      $clean = mb_convert_encoding( $dirty, 'UTF-8', 'UTF-8' );
+      $clean = htmlentities( $clean, ENT_QUOTES, 'UTF-8' );
+      $clean = htmlspecialchars_decode( $clean );
+      $clean = preg_replace( "/&#039;/", "'", $clean );
+      $clean = preg_replace( "/&quot;/", "\"", $clean );
+      return $clean;
+    }
+
     private static function removePrefix( &$item, $index, $start) {
       $item = substr( $item, $start );
     }
